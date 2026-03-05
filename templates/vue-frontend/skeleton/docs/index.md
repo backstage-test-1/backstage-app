afa9709d03e81fc763ad6c0fb51f0aa890623165
sqautboy <migonyoung01@gmail.com>
2026-03-05 16:18:36 +0900

    feat: add Vue.js template with CI/CD and Kubernetes manifests

---
+++ templates/vue-frontend/skeleton/docs/index.md
@@ -0,0 +1,16 @@
+# ${{ values.systemName }}-${{ values.name }}
+
+${{ values.description }}
+
+## Getting Started
+
+```bash
+npm install
+npm run dev
+```
+
+## Build
+
+```bash
+npm run build
+```
---
+++ templates/vue-frontend/skeleton/src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <div id="app">
+    <h1>{{ title }}</h1>
+    <p>${{ values.description }}</p>
+  </div>
+</template>
+
+<script setup>
+const title = '${{ values.systemName }}-${{ values.name }}'
+</script>
