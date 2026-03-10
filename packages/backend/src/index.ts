78ed2edb8fe89d65e7407733acd649e2cc37899a
sqautboy <migonyoung01@gmail.com>
2026-03-10 17:23:01 +0900

    Decouple the Backstage backend from the frontend bundle

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -2,7 +2,6 @@ import { createBackend } from '@backstage/backend-defaults';
 
 const backend = createBackend();
 
-backend.add(import('@backstage/plugin-app-backend'));
 backend.add(import('@backstage/plugin-proxy-backend'));
 
 // scaffolder plugin
